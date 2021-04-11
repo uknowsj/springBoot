2b183c9853b948ca4cbc123d7f74abd06e4c0d0c
uknowsj <44722864+uknowsj@users.noreply.github.com>
2021-04-11 19:10:04 +0900

    8-2 상품 API CRUD

---
+++ src/main/java/com/example/demo/controller/api/ItemApiController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller.api;
+
+import com.example.demo.ifs.CrudInterface;
+import com.example.demo.model.network.Header;
+import com.example.demo.model.network.request.ItemApiRequest;
+import com.example.demo.model.network.response.ItemApiResponse;
+import com.example.demo.service.ItemApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
+
+    @Override
+    @PostMapping("")
+    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+        return itemApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<ItemApiResponse> read(@PathVariable Long id) {
+
+        return itemApiLogicService.read(id);
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+
+        return itemApiLogicService.update(request);
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header delete(@PathVariable Long id) {
+
+        return itemApiLogicService.delete(id);
+    }
+}
